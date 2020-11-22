bc73e5af9e094212c49addc7b3d93f210305b7cb
JulioAv <avi19285@uvg.edu.gt>
2020-11-22 12:21:25 -0600

    1

--- uP.v
+++ uP.v
@@ -1,12 +1,12 @@
-module PC (input wire clock, enabled1, load, reset, input wire [11:0]D1, output reg [11:0]Q1);
+module PC (input wire clock, reset, enabled, load, input wire [11:0]D, output reg [11:0]Q);
 always @(posedge clock or posedge reset) begin
 
   if (reset)
-    Q1<=0;
+    Q<=0;
   else if (load)
-    Q1<=D1;
-  else if (enabled1)
-    Q1<=Q1+1'b1;
+    Q<=D;
+  else if (enabled)
+    Q<=Q+12'b1;
   end
 
 endmodule
@@ -22,21 +22,28 @@ end
 
 endmodule
 
-module Fetch (input wire [7:0]D2, input wire enabled2, clock, reset, output reg [3:0]op, ins);
-reg [7:0]Q2;
-always @(posedge clock or posedge enabled2 or posedge reset) begin
+module FFD4 (input wire clock, reset, enabled, input wire [3:0]D, output wire [3:0]Q);
+
+FFD1 M3 (clock, reset, enabled, D[0], Q[0]);
+FFD1 M4 (clock, reset, enabled, D[1], Q[1]);
+FFD1 M5 (clock, reset, enabled, D[2], Q[2]);
+FFD1 M7 (clock, reset, enabled, D[3], Q[3]);
+
+endmodule
+
+module FFD1 (input wire clock, reset, enabled, D, output reg Q);
+always @(posedge clock or posedge reset) begin
+
   if (reset)
-    Q2<=0;
-  else if (enabled2)
-    Q2<=D2;
-
-    assign ins[0] = Q2[4]; assign ins[1] = Q2[5]; assign ins[2] = Q2[6]; assign ins[3] = Q2[7];
-    assign op[0] = Q2[0]; assign op[1] = Q2[1]; assign op[2] = Q2[2]; assign op[3] = Q2[3];
+    Q<=0;
+  else if (enabled)
+    Q<=D;
   end
+endmodule
 
-
-
-
+module Fetch (input wire clock, reset, enabled, input wire [7:0]D, output wire[3:0]op, ins);
+FFD4 m1(clock, reset, enabled, D[7:4], op[3:0]);
+FFD4 m9(clock, reset, enabled, D[3:0], ins[3:0]);
 endmodule
 
 module Decode (input wire [6:0]D, output reg [12:0]Q);
@@ -71,49 +78,47 @@ end
 
 endmodule
 
-module ALU (input wire [3:0]A, B, input wire [2:0]F, output reg [3:0]Y, output reg FC, FZ);
-  always @(A, B, F) begin
+module ALU (input [3:0]A, B, input [2:0]F, output [3:0]Y, output  FC, FZ);
+  reg [4:0]S;
+  always @(A, B, F)
     case (F)
-      3'b000: Y= A;
-      3'b001: Y= A - B;
-      3'b010: Y= B;
-      3'b011: Y= A + B;
-      3'b100: Y= {1'b0, ~(A & B)};
-      default: Y= 5'b10101;
+      3'b000: S<= A;
+      3'b001: S<= A - B;
+      3'b010: S<= B;
+      3'b011: S<= A + B;
+      3'b100: S<= {1'b0, ~(A & B)};
+      default: S<= 5'b10101;
     endcase
 
-    assign FC = Y[4];
-    assign FZ = ~(Y[3] | Y[2] | Y[1] | Y[0]);
-    end
+    assign Y = S[3:0];
+    assign FC = S[4];
+    assign FZ = ~(S[3] | S[2] | S[1] | S[0]);
+
 endmodule
 
 module Bus(input wire [3:0]D, input wire B, output wire [3:0]Q);
   assign Q = B ? D : 4'bz;
 endmodule
 
-module Accu(input wire [3:0]D1, input wire enabled3, clock, reset, output reg [3:0]Q1);
-always @(posedge clock or posedge enabled3 or posedge reset) begin
-
-  if (reset)
-    Q1<=0;
-  else if (enabled3)
-    Q1<=D1;
-  end
+module Accu(input wire clock, reset, enabled, input wire [3:0]D, output wire [3:0]Q);
+FFD4 F1(clock, reset, enabled, D, Q);
 endmodule
 
-module Phase(input wire clock, reset, output reg Q);
+module Phase(input wire clock, reset, enabled, output reg Q);
 always @(posedge clock or posedge reset) begin
-if (reset)
+if (reset) begin
   Q<=0;
-else
+  end
+else if (enabled) begin
     Q<=~Q;
+    end
 
 end
 endmodule
 
 
-module Out(input wire [3:0]D, input wire clock, enabled, reset, output reg [3:0]Q);
-always @(posedge clock or posedge reset or posedge enabled) begin
+module Out(input wire clock, reset, enabled, input wire [3:0]D, output reg [3:0]Q);
+always @(posedge clock or posedge reset) begin
 if (reset)
   Q<=0;
 else if (enabled)
@@ -126,7 +131,7 @@ module RAM(
 	reg [3:0] dataO;
 	reg [3:0] mem [0:4095];
 
-	assign data = (cs & !we)? dataO: 4'bz;
+	assign data = (cs & !we)? dataO: 8'bz;
 
 	always @ (dir or data or cs or we)
 	begin
@@ -144,20 +149,16 @@ module RAM(
 
 endmodule
 
-module Flgs(input wire clock, reset, enabled, input wire [1:0]D, output reg [1:0]Q);
-always @(posedge clock or posedge reset or posedge enabled) begin
-if (reset)
-  Q<=0;
-else if (enabled)
-  Q<=D;
-end
+module Flgs(input wire clock, reset, enabled, input wire [1:0]D, output wire [1:0]Q);
+FFD1 m6(clock, reset, enabled, D[0], Q[0]);
+FFD1 m8(clock, reset, enabled, D[1], Q[1]);
 endmodule
 
 module uP(input wire clock, reset, input wire [3:0]pushbuttons, output wire phase, c_flag, z_flag,
           output wire [3:0]instr, oprnd, accu, data_bus, FF_out, output wire [7:0] program_byte,
           output wire [11:0]PC, address_RAM);
 
-wire[3:0] O;
+wire[3:0] O_ALU;
 wire[6:0] decode_in;
 wire[12:0] decode_out;
 wire C, ZERO;
@@ -165,16 +166,16 @@ wire C, ZERO;
 assign decode_in = {instr, c_flag, z_flag, phase};
 assign address_RAM={oprnd, program_byte};
 
-PC PC1(clock, decode_out[12], decode_out[11], reset, address_RAM, PC);
+PC PC1(clock, reset, decode_out[12], decode_out[11], address_RAM, PC);
 ROM ROM1(PC, program_byte);
-Fetch Fetch1(program_byte, ~phase, clock, reset, oprnd, instr);
+Fetch Fetch1(clock, reset, ~phase, program_byte, oprnd, instr);
 Decode Decode1(decode_in, decode_out);
 Bus bus_1(oprnd, decode_out[1], data_bus);
-ALU ALU6(accu, data_bus, decode_out[8:6], O, C, ZERO);
-Accu ACCU7(O, decode_out[10], clock, reset, accu);
-Bus bus_2(O,  decode_out[3], data_bus);
-Phase PHase9(clock, reset, phase);
-Out Out0(data_bus, clock, decode_out[0], reset, FF_out);
+ALU ALU6(accu, data_bus, decode_out[8:6], O_ALU, C, ZERO);
+Accu ACCU7(clock, reset, decode_out[10], O_ALU, accu);
+Bus bus_2(O_ALU, decode_out[3], data_bus);
+Phase PHase9(clock, reset, 1'b1, phase);
+Out Out0(clock, reset, decode_out[0], data_bus, FF_out);
 Bus in(pushbuttons, decode_out[2], data_bus);
 RAM ram1(decode_out[5], decode_out[4], address_RAM, data_bus);
 Flgs f1(clock, reset, decode_out[9], {C, ZERO}, {c_flag, z_flag});
